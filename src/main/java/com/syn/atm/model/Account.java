de9c355bff0830108d60609fd1ce749a127db23b
sylvianikol <silviaadvert@gmail.com>
2021-03-13 15:34:10 +0200

    tests added

--- src/main/java/com/syn/atm/model/Account.java
+++ src/main/java/com/syn/atm/model/Account.java
@@ -1,5 +1,26 @@
 package com.syn.atm.model;
 
+
+import java.math.BigDecimal;
+
 public class Account {
 
+    private String id;
+    private BigDecimal balance;
+
+    public Account() {
+    }
+
+    public Account(String id, BigDecimal balance) {
+        this.id = id;
+        this.balance = balance;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public BigDecimal getBalance() {
+        return this.balance;
+    }
 }
